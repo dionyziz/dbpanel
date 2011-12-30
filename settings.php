940884ad5b7dead9f9ca80b4a59bf360765e261e
Dionysis Zindros <dionyziz@gmail.com>
2011-12-30 01:01:47 +0000

    local settings suport

--- settings.php
+++ settings.php
@@ -1,5 +1,14 @@
 <?php
-    return array(
-        'url' => 'http://mariosal.com/dbpanel/'
+    $settingsLocal = array();
+    if ( file_exists( 'settings-local.php' ) ) {
+        $settingsLocal = include 'settings-local.php';
+    }
+    $settingsGlobal = array(
+        'url' => 'http://example.com/'
     );
+
+    foreach ( $settingsLocal as $key => $setting ) {
+        $settingsGlobal[ $key ] = $setting;
+    }
+    return $settingsGlobal;
 ?>
